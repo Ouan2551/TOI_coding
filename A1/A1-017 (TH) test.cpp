cc01a47c7b6a250b6491e3d0267c10ea5179ac3f
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-05 22:55:35 +0700

    -

---
+++ A1/A1-017 (TH) test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int y1, m1, d1, y2, m2, d2; y1 = m1 = d1 = y2 = m2 = d2 = 0;
+    cin >> y1 >> m1 >> d1 >> y2 >> m2 >> d2;
+    if (y1 == y2 && m1 == m2 && d1 == d2)
+    {
+        cout << 0;
+    }
+    else if (y1 > y2)
+    {
+        cout << 2;
+    }
+    else if (m1 > m2)
+    {
+        cout << 2;
+    }
+    else if (d1 > d2)
+    {
+        cout << 2;
+    }
+    else
+    {
+        cout << 1;
+    }
+    return 0;
+}
