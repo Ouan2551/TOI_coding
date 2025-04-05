b49ebd431ceb596314cfc7ceb614a7afb060acb5
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-05 16:28:46 +0700

    -

--- A1/A1-012 (TH).cpp
+++ A1/A1-012 (TH).cpp
@@ -5,29 +5,28 @@ int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     string num1, num2; cin >> num1; char op_used; cin >> op_used;
-    vector<int> change; int count = num1.size(), result = 0, carry = 0;
+    vector<int> change; int count = num1.size(), result = 0;
     for (int i = count - 1; i >= 0; i--)
     {
         change.push_back(num1[i]);
     }
     for (int i = 0; i < count; i++)
     {
+        if (change[i] == '0')
+        {
+            continue;
+        }
         num2 += change[i];
     }
-    for (int i = 0; i < count; i++)
+    if (op_used == '+')
     {
-        if (op_used == '+')
-        {
-            result = (num1[i] - '0') + (num2[i] - '0');
-            int result = (result % 10) + carry, carry = result / 10;
-            cout << result;
-        }
-        else if (op_used == '*')
-        {
-            result = (num1[i] - '0') * (num2[i] - '0');
-            cout << result;
-        }
+        result = stoi(num1) + stoi(num2);
+        cout << num1 << " + " << num2 << " = "<< result;
     }
-    
+    else if (op_used == '*')
+    {
+        result = stoi(num1) * stoi(num2);
+        cout << num1 << " * " << num2 << " = " << result;
+    }
     return 0;
 }
