ed2d687b118198728b0e48f165a975bb5265f98d
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-13 23:32:25 +0700

    -

--- A1/A1-031 (TH).cpp
+++ A1/A1-031 (TH).cpp
@@ -3,6 +3,14 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    
+    string num; cin >> num; int count = num.size();
+    for (int i = count; i >= 0; i--)
+    {
+        cout << num[i];
+        if (i % 3 == 0 && i != 0)
+        {
+            cout << ',';
+        }
+    }
     return 0;
 }
