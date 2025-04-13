748121f92ffc14ca6f3aef695f35b2d5cbe349b5
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-14 00:01:45 +0700

    -

--- A1/A1-031 (TH).cpp
+++ A1/A1-031 (TH).cpp
@@ -1,16 +1,40 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     string num; cin >> num; int count = num.size();
-    for (int i = count; i >= 0; i--)
-    {
-        cout << num[i];
-        if (i % 3 == 0 && i != 0)
+    // if (count < 7)
+    // {
+        int position = count - 4;
+        for (int i = 0; i < count; i++)
         {
-            cout << ',';
+            cout << num[i];
+            if (i == position)
+            {
+                cout << ',';
+            }
         }
-    }
+    // }
+    // this for who want size >= 7
+    // else
+    // {
+    //     vector<int> position;int chk_position = count - 4;
+    //     // find position
+    //     while (chk_position > 0)
+    //     {
+    //         position.push_back(chk_position); chk_position -= 3;
+    //     }
+    //     int index = position.size() - 1;
+    //     for (int i = 0; i < count; i++)
+    //     {
+    //         cout << num[i];
+    //         if (i == position[index])
+    //         {
+    //             cout << ','; index--;
+    //         }
+    //     }
+    // }
     return 0;
 }
