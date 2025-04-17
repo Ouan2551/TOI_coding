d81a689cb7921c1f93881d0841f5f58ed3aa8092
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-17 16:57:47 +0700

    -

--- A2/A2-004 (TH).cpp
+++ A2/A2-004 (TH).cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
+#include <climits>
 using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     vector<int> dish; int count = 0, num; cin >> count;
-    vector<vector<int>> result;
+    unordered_map<int, int> check;
     for (int i = 0; i < count; i++)
     {
         cin >> num; dish.push_back(num);
@@ -14,7 +16,15 @@ int main()
     sort(dish.begin(), dish.end());
     for (int i = 0; i < count; i++)
     {
-        
+        int num = dish[i];
+        check[num] += 1;
     }
+    int max_val = INT_MIN;
+    for (int i = 0; i < count; i++)
+    {
+        int num = dish[i];
+        max_val = max(max_val, check[num]);
+    }
+    cout << max_val;
     return 0;
 }
