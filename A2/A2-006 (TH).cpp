387a8dddd64fd0916cf5480646aa9128b241ad90
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-20 15:56:06 +0700

    -

--- A2/A2-006 (TH).cpp
+++ A2/A2-006 (TH).cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+int change_direction(int x, int y, int dx, int dy, int count)
+{
+    if (x == count - 1)
+    {
+        x = 
+    }
+    return x, y, dx, dy;
+}
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
@@ -19,6 +27,32 @@ int main()
     if (check_count == count*count)
     {
         cout << count*count; return 0;
+    }
+    int x = 0, y = 0, dx = 0, dy = 0;
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j < count; j++)
+        {
+            // check start point
+            if (path[x][y] == 'X')
+            {
+                continue;
+            }
+            // check that can go to finished point
+            while (dx == count - 1 && dy == count - 1)
+            {
+                // check right
+                if (path[dx][dy+1] == '.')
+                {
+                    dy = dy + 1;
+                }
+                // check down
+                else if (path[dx+1][dy] == '.')
+                {
+                    dx = dx + 1;
+                }
+            }
+        }
     }
     return 0;
 }
