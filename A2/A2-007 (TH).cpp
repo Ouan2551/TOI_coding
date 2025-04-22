1e8c0d772fe83d2ac79dd844441cc99ef8e9acd7
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-22 23:09:21 +0700

    -

--- A2/A2-007 (TH).cpp
+++ A2/A2-007 (TH).cpp
@@ -1,8 +1,21 @@
 #include <iostream>
-using namespace std;
+#include <vector>
 int main()
 {
-    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-
+    std::ios::sync_with_stdio(0);std::cin.tie(0);std::cout.tie(0);
+    int N, M, num; std::cin >> N >> M; std::vector<int> bucket_select;
+    std::vector<std::vector<int>> location_bucket(N, std::vector<int>(2));
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            std::cin >> location_bucket[i][j];
+        }
+    }
+    for (int i = 0; i < M; i++)
+    {
+        std::cin >> num; bucket_select.push_back(num);
+    }
+    
     return 0;
 }
