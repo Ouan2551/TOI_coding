148cdbe752a4b32bbc86d26a7097666780330977
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-05-07 23:22:55 +0700

    -

--- A2/A2-009 (TH).cpp
+++ A2/A2-009 (TH).cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 int main()
 {
     std::ios::sync_with_stdio(0);std::cin.tie(0);std::cout.tie(0);
-    int group = 0, card_group = 0, round = group/2; std::cin >> group >> card_group;
+    int group = 0, card_group = 0, lost = 0; std::cin >> group >> card_group;
+    int round = group/2;
     std::vector<std::vector<int>> table(group, std::vector<int>(group));
     std::vector<int> winner; std::vector<int> win_round;
-    std::cout << "wow";
     for (int i = 0; i < group; i++)
     {
         for (int j = 0; j < group; j++)
@@ -19,17 +20,35 @@ int main()
     while (all_round_count < group/2)
     {
         int plus = 0;
-        for (int i = 0; i < round; i++)
+        // first round
+        if (all_round_count == 0)
         {
-            int win = table[i+plus][i+1+plus];
-            win_round.push_back(win); plus++;
+            for (int i = 0; i < round; i++)
+            {
+                int x = winner[i] - 1, y = winner[i+1] - 1;
+                int win = table[x][y];
+                win_round.push_back(win); plus++;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < round; i++)
+            {
+                int x = win_round[i] - 1, y = win_round[i+1] - 1;
+                int win = table[x][y];
+                if (x != win)
+                {
+                    lost = x;
+                }
+                else if (y != win)
+                {
+                    lost = y;
+                }
+                win_round.erase(std::find(winner.begin(), winner.end(), lost));
+            }
         }
         all_round_count++; round /= 2;
     }
-    // for (int i = 0; i < winner.size(); i++)
-    // {
-    //     std::cout << winner[ << '\n';
-    // }
-    std::cout << win_round.back();
+    std::cout << win_round.front();
     return 0;
 }
